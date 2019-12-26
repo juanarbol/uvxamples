a79d98c96b10b66137cb89ae64d9cf6f712fcedc
Juan José Arboleda <soyjuanarbol@gmail.com>
2019-12-26 17:01:26 -0500

    example: improves signal implementation

--- src/signals/signal.cc
+++ src/signals/signal.cc
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include <uv.h>
+#include <cstdlib>
+
+static int kHandleTimes = 1;
 
 void signal_handler(uv_signal_t* handle, int signum) {
+  kHandleTimes--;
+
   // Print that our signal was received
   printf("Signal catched: %d\n", signum);
 
-  // Stop our signal listening
-  uv_signal_stop(handle);
+  if (kHandleTimes == 0) {
+    // Stop our signal listening
+    // at this point, the process will finish.
+    uv_signal_stop(handle);
+  }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+  // Handle signal k times using argv.
+  if (argc > 1) {
+    kHandleTimes = atoi(argv[1]);
+  }
+
   // Initialize our event loop
   uv_loop_t* loop = uv_default_loop();
 
   // Create a variable for our signal handler
   uv_signal_t signal;
 
-  // Initialize signal handler
+  // Initialize signal handler.
   uv_signal_init(loop, &signal);
-  // In this case, we'll listening to (SIGINT or crt + c) signal
-  // When that signal is triggered, will run signal_handler callback
+
+  // In this case, we'll listening to SIGINT (ctrl + c) signal.
+  // When that signal is triggered, will run signal_handler callback.
   uv_signal_start(&signal, signal_handler, SIGINT);
 
   // Run our event loop
